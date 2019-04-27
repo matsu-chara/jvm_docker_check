b5b5ed5cc59351d8017f58d41ac752ce7a35bef5
matsu-chara <matsuy00@gmail.com>
2019-04-28 01:34:36 +0900

    gen dockerfile

--- run.sh
+++ run.sh
@@ -11,6 +11,19 @@ __go() {
   docker run --rm "$img"
 }
 
+__create_df() {
+  image="$1"
+  df="$2"
+  cat <<EOS > "$df"
+FROM $image
+
+WORKDIR /home
+COPY Print.java .
+RUN javac Print.java
+ENTRYPOINT java Print
+EOS
+}
+
 __build() {
   img="$1"
   df="$2"
@@ -22,6 +35,12 @@ __rm() {
 
   docker rmi "$img"
 }
+
+__create_df "amazoncorretto:8u212" "DockerfileCorreto8u212"
+__create_df "openjdk:8u212" "DockerfileOpen8u212"
+__create_df "amazoncorretto:8u192" "DockerfileCorreto8u192"
+__create_df "openjdk:8u181" "DockerfileOpen8u181"
+__create_df "openjdk:8u102" "DockerfileOpen8u102"
 
 __build "jvm-test-correto8u212" "DockerfileCorreto8u212"
 __build "jvm-test-open8u212" "DockerfileOpen8u212"
